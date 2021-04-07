2cf67723326dfffd8f951eca926b36ca391c184d
Siyu Zhou <siyu.zhou@outlook.com>
2021-04-06 22:29:08 -0700

    Update `layer_output.py` interface.

--- layer_output.py
+++ layer_output.py
@@ -16,15 +16,14 @@ def main():
     model_params = swarmnet.utils.load_model_params(ARGS.config)
 
     # data contains edge_types if `edge=True`.
-    data = load_data(ARGS.data_dir, ARGS.data_transpose,
-                     prefix=prefix)
+    data = load_data(ARGS.data_dir, prefix=prefix)
     print(f"\nData from {ARGS.data_dir} loaded.")
 
     # input_data: a list which is [time_segs, edge_types] if `edge_type` > 1, else [time_segs]
     input_data, expected_time_segs = preprocess_data(
-        data, model_params['time_seg_len'], ARGS.pred_steps, edge_type=model_params['edge_type'])
+        data, model_params['time_seg_len'], ARGS.pred_steps, edge_type=model_params['edge_type'], ground_truth=False)
     print(f"Data processed.\n")
-    nagents, ndims = expected_time_segs.shape[-2:]
+    nagents, ndims = data[0].shape[-2:]
 
     model, inputs = swarmnet.SwarmNet.build_model(
         nagents, ndims, model_params, pred_steps=ARGS.pred_steps, return_inputs=True)
@@ -37,15 +36,19 @@ def main():
 
     # Create Debug model
     outlayer_name = ARGS.layer_name
+    layers = {'edge_encoder': model.graph_conv.edge_encoder,
+              'edge_aggr': model.graph_conv.edge_aggr,
+              'node_decoder': model.graph_conv}
 
     outlayer_model = keras.Model(
-        inputs=inputs, outputs=model.get_layer(outlayer_name).output)
+        inputs=inputs, outputs=layers[outlayer_name].output)
 
     print(f"\nOutput up to {outlayer_name}\n")
     outlayer_model.summary()
 
     layer_output = outlayer_model.predict(input_data)
-    np.save(os.path.join(ARGS.log_dir, f'{outlayer_name}_output'), layer_output)
+    np.save(os.path.join(ARGS.log_dir,
+            f'{outlayer_name}_output'), layer_output)
     print(f"Layer {outlayer_name} output saved.")
 
 
@@ -53,8 +56,6 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--data-dir', type=str,
                         help='data directory')
-    parser.add_argument('--data-transpose', type=int, nargs=4, default=None,
-                        help='axes for data transposition')
     parser.add_argument('--config', type=str,
                         help='model config file')
     parser.add_argument('--log-dir', type=str,
